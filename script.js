1fbaa169474e8779c50420af8ad81e892b99a4cb
Ellapaw <pawlowe@miamioh.edu>
2025-02-25 18:05:17 -0500

    draft

---
+++ script.js
@@ -0,0 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("drawer-toggle-btn");
+    const drawer = document.getElementById("drawer");
+
+    button.addEventListener("click", function () {
+        drawer.classList.toggle("show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("drawer2-toggle-btn");
+    const drawer = document.getElementById("drawer2");
+
+    button.addEventListener("click", function () {
+        drawer.classList.toggle("show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("drawer3-toggle-btn");
+    const drawer = document.getElementById("drawer3");
+
+    button.addEventListener("click", function () {
+        drawer.classList.toggle("show");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("drawer4-toggle-btn");
+    const drawer = document.getElementById("drawer4");
+
+    button.addEventListener("click", function () {
+        drawer.classList.toggle("show");
+    });
+});
